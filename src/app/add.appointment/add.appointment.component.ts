41e3318255efd3745fd8974075f24f425bba3a2a
Bohdan Tsap <bohdantsap@Bohdans-MacBook-Pro.local>
2024-10-26 15:13:46 +0300

    wip

--- src/app/add.appointment/add.appointment.component.ts
+++ src/app/add.appointment/add.appointment.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add.appointment',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './add.appointment.component.html',
   styleUrl: './add.appointment.component.css'
 })
 export class AddAppointmentComponent {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required], // Name field is required
+      email: ['', [Validators.required, Validators.email]], // Email field with validation
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value); // Handle the form submission
+    }
+  }
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { CalendarComponent } from './calendar/calendar.component';
 import { DayComponent } from './day/day.component';
 import { AddAppointmentComponent } from './add.appointment/add.appointment.component';
 
