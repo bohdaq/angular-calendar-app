ad4a2b0cfc946fb78ee09e5ea492075c78900cb4
Bohdan Tsap <bohdantsap@Bohdans-MacBook-Pro.local>
2024-10-26 13:20:47 +0300

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import { DayComponent } from './day/day.component';
+import { AddAppointmentComponent } from './add.appointment/add.appointment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+    { path: 'home', component: CalendarComponent },
+    { path: 'day', component: DayComponent },
+    { path: 'add', component: AddAppointmentComponent },
+];
