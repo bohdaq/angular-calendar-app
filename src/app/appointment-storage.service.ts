f9d695f1637a844796e02fbcc34d574af526b116
Bohdan Tsap <bohdantsap@Bohdans-MacBook-Pro.local>
2024-10-28 00:52:25 +0200

    wip

--- src/app/appointment-storage.service.ts
+++ src/app/appointment-storage.service.ts
@@ -7,42 +7,42 @@ import { Appointment } from './day/day.component';
 export class AppointmentStorageService {
 
   getAppointments(day: string): Appointment[] {
-    let appointmentList = this.populateEmptyFields(day);
+    const appointmentList = this.populateEmptyFields(day);
 
-    let appointments: Appointment[] = appointmentList[day].items;
+    const appointments: Appointment[] = appointmentList[day].items;
     return appointments;
   }
 
   addAppointment(day: string, appointment: Appointment) {
-    let appointmentList = this.populateEmptyFields(day);
+    const appointmentList = this.populateEmptyFields(day);
 
     appointmentList[day].items.push(appointment);
     localStorage.setItem('appointmentList', JSON.stringify(appointmentList));
   }
 
   setAppointments(day: string, appointments: Appointment[]) {
-    let appointmentList = this.populateEmptyFields(day);
+    const appointmentList = this.populateEmptyFields(day);
 
     appointmentList[day].items = appointments;
     localStorage.setItem('appointmentList', JSON.stringify(appointmentList));
   }
 
   deleteAppointment(day: string, index: number) {
-    let appointmentList = this.populateEmptyFields(day);
+    const appointmentList = this.populateEmptyFields(day);
 
     appointmentList[day].items.splice(index, 1);
     localStorage.setItem('appointmentList', JSON.stringify(appointmentList));
   }
 
   getCalendar() {
-    let json: string = localStorage['appointmentList'] || '{}';
-    let appointmentList = JSON.parse(json) || {};
+    const json: string = localStorage['appointmentList'] || '{}';
+    const appointmentList = JSON.parse(json) || {};
 
     return appointmentList;
   }
 
   populateEmptyFields(day: string) {
-    let appointmentList = this.getCalendar();
+    const appointmentList = this.getCalendar();
 
     if (!appointmentList[day]) {
       appointmentList[day] = {
